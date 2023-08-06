171d9964f23a045b064cc7d9d64ff9bbb161214e
Guilherme Chagas <gsagahc@hotmail.com>
2023-08-06 14:50:32 -0300

    Projeto de controle de alergias de prontuário
    Projeto Spring Boot :Banco Oracle, Jsonwebtoken, Native Query, Java Security, Lombok

--- Alergias/src/main/java/com/alergias/prontuario/AlergiasApplication.java
+++ Alergias/src/main/java/com/alergias/prontuario/AlergiasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlergiasApplication {
-
+   /*Projeto de Alergias de prontuário Base Oracle*/
 	public static void main(String[] args) {
 		SpringApplication.run(AlergiasApplication.class, args);
 	}
