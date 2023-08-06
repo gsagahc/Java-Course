cd5139e744e529cecdf6a689c321008dcc847302
Guilherme Chagas <gsagahc@hotmail.com>
2023-08-06 14:56:12 -0300

    Projeto Parking Control
    Projeto Spring Boot, banco postgresql

--- parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class ParkingControlApplication {
-
+   /*Poejeto parking control*/
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);}
 		@GetMapping("/") 
